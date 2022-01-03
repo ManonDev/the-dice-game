f47da6b1d206c3bf466918e57c862799461059fb
Manon Bayer <manonbayer.pro@gmail.com>
2022-01-03 14:23:38 +0100

    add coms

--- script.js
+++ script.js
@@ -2,16 +2,23 @@
 let player1 = document.querySelector('#player-1');
 let player2 = document.querySelector('#player-2');
 let form = document.querySelector('#form');
+let scoresGlobal;
+let roundScore;
+let gameStart;
 
-
+// Function NEW GAME
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     e.stopPropagation();
+    // ask for the names of the players
     player1.textContent = document.querySelector('#name-1').value;
     player2.textContent = document.querySelector('#name-2').value;
 
+    // Close the modal if the names of the players are indicated
     if(player2.value != "" && player2.value != "") {
         $('#modal-new-game').modal('hide');
     }
+ 
+
 });
 
